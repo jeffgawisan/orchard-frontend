d099c3997313563777c9eb771f3569e02ad17b2d
Jefferson Gawisan <Jefferson.Garrison@firstmac.com.au>
2023-04-17 12:50:47 +0800

    top component data content and display styles desktop and mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.mobile.scss', './app.component.desktop.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'skill-test';
-  pageContent = {
-    title: "ANSWER YOUR BODY'S NEED",
-    description: "The way ingredients are sourced affects the way nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume accourding to your body's needs",
-    moreDetails: {
-      title: 'BE MINDFUL',
-      description: "Sourcing lolcal or organic food is a good way to start being more mindful about what your'e cooking and eating."
-    },
-    images: [
-      { desktop: "/assets/images/component-01/Image-01@2x.jpg", mobile: "/assets/images/component-01/Image-01.jpg" },
-      { desktop: "/assets/images/component-01/Image-02@2x.jpg", mobile: "/assets/images/component-01/Image-02.jpg" },
-      { desktop: "/assets/images/component-01/Image-03@2x.jpg", mobile: "/assets/images/component-01/Image-03.jpg" }
-    ]
-  }
+    title = 'skill-test';
+    featuredContent = {
+        title: "ANSWER YOUR BODY'S NEED",
+        description:
+            "The way ingredients are sourced affects the way nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume accourding to your body's needs",
+        moreDetails: {
+            title: 'BE MINDFUL',
+            description:
+                "Sourcing local or organic food is a good way to start being more mindful about what your'e cooking and eating.",
+        },
+        images: [
+            {
+                desktop: '/assets/images/component-01/Image-01@2x.jpg',
+                mobile: '/assets/images/component-01/Image-01.jpg',
+            },
+            {
+                desktop: '/assets/images/component-01/Image-02@2x.jpg'  ,
+                mobile: '/assets/images/component-01/Image-02.jpg',
+            },
+            {
+                desktop: '/assets/images/component-01/Image-03@2x.jpg',
+                mobile: '/assets/images/component-01/Image-03.jpg',
+            },
+        ],
+    };
+
 }
